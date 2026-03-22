8f1f954b7c8fb60486bac4a30dabc985e8b9ae45
Shukurulloh92 <122691479+Shukurulloh92@users.noreply.github.com>
2026-03-22 23:59:16 +0500

    Add manifest and sw

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'foiz-kalk-v1';
+const ASSETS = [
+  '/compound-calculator/',
+  '/compound-calculator/index.html',
+  '/compound-calculator/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/compound-calculator/')))
+  );
+});
